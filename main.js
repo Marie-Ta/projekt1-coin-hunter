cd18352a7f77382088f91278185b069522f50c8a
Marie Tarantova <marie.tarantova@gmail.com>
2021-03-30 17:33:06 +0200

    Nastavený  pohyb panáčka a generování nové mince. Panáček ještě vychází z okna a neumí sebrat minci.

--- main.js
+++ main.js
@@ -45,16 +45,12 @@ function priNacteniStranky() {
 	console.log ("panacekVyska " + panacekVyska);
 
 	// a umístíme panáčka do středu okna
-	
-	panacekX = (oknoSirka/2)-(panacekSirka/2);
+	panacekX = Math.floor((oknoSirka/2)-(panacekSirka/2));
 	panacek.style.left = panacekX + 'px';
 	
-	panacekY =  (oknoVyska/2)-(panacekVyska/2);
+	panacekY = Math.floor((oknoVyska/2)-(panacekVyska/2));
 	panacek.style.top = panacekY + 'px';
 	
-	//panacek.style.top = (oknoVyska/2)-(panacekVyska/2) + 'px';
-
-
 	// umístíme panáčka na startovní pozici (MT: to je do středu okna, ne?)
 
 	// zjistíme šířku a výšku mince
@@ -90,125 +86,70 @@ function umistiPanacka() {
 
 // funkce pro nahodné vygenerování nové pozice mince
 // a umístění mince na tyto souřadnice
+
 function novaMince() {
 	// musíme to napsat :)
+	minceX = Math.floor((Math.random())*(oknoSirka-minceSirka));
+	
+	console.log (minceX);
+
+	mince.style.left = minceX + 'px';
+
+	minceY = Math.floor((Math.random())*(oknoVyska-minceVyska));
+	
+	console.log ("nova mince");
+
+	mince.style.top = minceY + 'px';
 }
 
 // tato funkce se zavolá při stisku klávesy
-// do proměnné "udalost" se vloží objekt s parametry události¨
+// do proměnné "udalost" se vloží objekt s parametry události
 // kde lze najít např. i vlastnost "key",
 // která obsahuje znak stisknuté klávesy
 function priStiskuKlavesy(udalost) {
 
-	// šipka vlevo
+	let klavesa = udalost.key;
 
+	// šipka vlevo
+	if(klavesa === "ArrowLeft"){
+		panacekX= panacekX - 5;
+		panacek.style.left = panacekX + 'px';
+		console.log("doleva " +panacekX);
+	}
 
 	// šipka vpravo
-
+	if(klavesa === "ArrowRight"){
+		panacekX= panacekX + 5;
+		panacek.style.left = panacekX + 'px';
+		console.log("doprava " +panacekX);
+	}
 
 	// šipka nahoru
-
+	else if (klavesa == "ArrowUp"){
+		panacekY= panacekY - 5;
+		panacek.style.top = panacekY + 'px';
+		console.log("nahoru "+panacekY);
+	}
 
 	// šipka dolů
-
-
+	else if (klavesa == "ArrowDown"){
+		panacekY= panacekY + 5;
+		panacek.style.top = panacekY + 'px';
+		console.log("dolu "+panacekY);
+	}
+	
 	// panáčka umistíme na nově vypočítanou pozici
 
 
 	// otestujeme kolizi panáčka s mincí
-
-
+	
 }
 
 // fuknce pro otestování kolize panáčka s mincí
 function otestujKolizi() {
 	// musíme to napsat :)
+	if(panacekY==minceY && panacekX==minceX){
+		console.log("mince v kapse!");
+		novaMince();
+	} 
 }
-
-
-
-
-
-/*
-
-let panacek, panacekX, panacekY, panacekSirka, panacekVyska;
-let mince, minceX, minceY, minceSirka, minceVyska;
-
-panacek = document.querySelector("#panacek");
-mince = document.querySelector("#mince");
-
-/*function test (elementSelector, positionChange){
-let element = document.querySelector(elementSelector);
-let currentTop = parseInt(element.style.top)
-element.style.top = (currentTop + positionChange) + "px";
-}*/
-
-
-
-// POHYB PANÁČKA
-
-
-
-
-//let souradniceTop = 250;
-//let souradniceLeft = 550;
-
-/*
-let souradniceTop = parseInt(panacek.style.top, 10);
-let souradniceLeft = parseInt(panacek.style.left, 10);
-
-console.log(souradniceTop)*/
-
-/*panacek.style.top = souradniceTop + 'px';
-panacek.style.left = souradniceLeft + 'px';*/
-/*
-function otestujKlavesu(udalost){
-	let klavesa = udalost.key;
-	
-	if (klavesa == "ArrowDown") {
-		
-		souradniceTop = (souradniceTop + 5);
-		//panacek.style.top = souradniceTop + 5 + "px";
-		
-	}
-
-	else if (klavesa === "ArrowUp") {
-		console.log("nahoru")
-		panacek.style.top = souradniceTop + -5 + "px";
-	}
-
-	else if (klavesa === "ArrowLeft") {
-		console.log("dolu")
-		panacek.style.left = souradniceTop + -5 + "px";
-	}
-
-	else if (klavesa === "ArrowRight") {
-		console.log("pravo")
-		panacek.style.left = souradniceTop + 5 + "px";
-	}
-
-	else{
-		console.log("jiná klávesa");
-	}
-
-}
-*/
-
-/*
-// zjistím výšku a šířku okna
-let sirkaOkna = window.innerWidth;
-let vyskaOkna = window.innerHeight
-console.log (sirkaOkna, vyskaOkna);
-//definování náhodné pozice top/left, generování náhodných čísel od 0 (min) do výšky/šířky okna (max)
-    let nahodnaPoziceTop;
-    let nahodnaPoziceLeft;
-    nahodnaPoziceTop = getRandomNumber(0, vyskaOkna)
-    nahodnaPoziceLeft = getRandomNumber(0, sirkaOkna)
-    console.log(nahodnaPoziceTop, nahodnaPoziceLeft);
-    //generování náhodné pozice
-    mince.style.top = nahodnaPoziceTop + 'px'
-    mince.style.left = nahodnaPoziceLeft + 'px'
-function getRandomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-}
-*/
