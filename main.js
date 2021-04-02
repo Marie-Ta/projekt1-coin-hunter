0dda1df7a81b65de51f30dca58fb2067a0fb9685
Marie Tarantova <marie.tarantova@gmail.com>
2021-04-02 20:05:30 +0200

    Přidáno přehrání fanfáry a vítězné hlášky, nastaveny obrázky na pohyb panáčka. Kód očištěn od console.logů

--- main.js
+++ main.js
@@ -21,13 +21,7 @@ let oknoSirka, oknoVyska
 let okno = document.querySelector(".js-body");
 
 oknoSirka = okno.offsetWidth;
-console.log ("sirka " + oknoSirka);
-	
 oknoVyska = okno.offsetHeight;
-console.log ("vyska " + oknoVyska);
-
-console.log ("nahodne cislo " + Math.random());
-
 
 // tato funkce se spustí při načtení stránky
 // tj. ve chvíli, kdy je načtené komplet HTML, CSS a všechny obrázky a zvuky
@@ -40,11 +34,8 @@ function priNacteniStranky() {
 
 	// zjistíme šířku a výšku panáčka
 	panacekSirka = panacek.offsetWidth;
-	console.log ("panacekSirka " + panacekSirka);
-
 	panacekVyska = panacek.offsetHeight;
-	console.log ("panacekVyska " + panacekVyska);
-
+	
 	// a umístíme panáčka do středu okna
 	panacekX = Math.floor((oknoSirka/2)-(panacekSirka/2));
 	panacek.style.left = panacekX + 'px';
@@ -56,23 +47,14 @@ function priNacteniStranky() {
 
 	// zjistíme šířku a výšku mince
 	minceSirka = mince.offsetWidth;
-	console.log ("minceSirka " + minceSirka);
-
 	minceVyska = mince.offsetHeight;
-	console.log ("minceVyska " + minceVyska);
 
 	// a vygenerujeme první minci na náhodné pozici
 
 	minceX = Math.floor((Math.random())*(oknoSirka-minceSirka));
-	
-	console.log (minceX);
-
 	mince.style.left = minceX + 'px';
 
 	minceY = Math.floor((Math.random())*(oknoVyska-minceVyska));
-	
-	console.log (minceY);
-
 	mince.style.top = minceY + 'px';
 
 }
@@ -80,8 +62,6 @@ function priNacteniStranky() {
 // funkce, která umístí panáčka na jeho souřadnice
 // tj. nastaví jeho style.left a style.top na hodnoty panacekX, panacekY
 function umistiPanacka() {
-	// musíme to napsat :)
-	
 }
 
 
@@ -89,17 +69,9 @@ function umistiPanacka() {
 // a umístění mince na tyto souřadnice
 
 function novaMince() {
-	// musíme to napsat :)
 	minceX = Math.floor((Math.random())*(oknoSirka-minceSirka));
-	
-	console.log (minceX);
-
 	mince.style.left = minceX + 'px';
-
 	minceY = Math.floor((Math.random())*(oknoVyska-minceVyska));
-	
-	console.log ("nova mince");
-
 	mince.style.top = minceY + 'px';
 }
 
@@ -115,49 +87,45 @@ function priStiskuKlavesy(udalost) {
 	if(klavesa === "ArrowLeft" && panacekX > 0){
 		panacekX= panacekX - 10;
 		panacek.style.left = panacekX + 'px';
-		console.log("doleva " +panacekX);
+		panacek.src="obrazky/panacek-vlevo.png"
 	}
 
 	// šipka vpravo
 	if(klavesa === "ArrowRight" && panacekX < (window.innerWidth - panacekSirka)){
 		panacekX= panacekX + 10;
 		panacek.style.left = panacekX + 'px';
-		console.log("doprava " +panacekX);
+		panacek.src="obrazky/panacek-vpravo.png"
 	}
 
 	// šipka nahoru
 	else if (klavesa == "ArrowUp" && panacekY > 0){
 		panacekY= panacekY - 10;
 		panacek.style.top = panacekY + 'px';
-		console.log("nahoru "+panacekY);
+		panacek.src="obrazky/panacek-nahoru.png"
 	}
 
 	// šipka dolů
 	else if (klavesa == "ArrowDown" && panacekY < (window.innerHeight - panacekVyska)){
 		panacekY= panacekY + 10;
 		panacek.style.top = panacekY + 'px';
-		console.log("dolu "+panacekY);
+		panacek.src="obrazky/panacek.png"
 	}
 	
-	// panáčka umistíme na nově vypočítanou pozici
-
-
 	// otestujeme kolizi panáčka s mincí
 	otestujKolizi();
 }
 
 // funkce pro otestování kolize panáčka s mincí - Koliduje okraj panáčka s mincí?
 function otestujKolizi() {
-	let pravyOkrajPanackaSeNedotyka = panacekX + panacekSirka < minceX;
+	/*let pravyOkrajPanackaSeNedotyka = panacekX + panacekSirka < minceX;
 	let levyOkrajPanackaSeNedotyka = minceX + minceSirka < panacekX;
 	let dolniOkrajPanackaSeNedotyka = panacekY + panacekVyska < minceY;
-	let horniOkrajPanackaSeNedotyka = minceY + minceVyska < panacekY;
+	let horniOkrajPanackaSeNedotyka = minceY + minceVyska < panacekY;*/
 
 	// jednotlivé podmínky (proměnné výše si můžu přepsat do podmínky níže. Otazník na začátku znamená negaci )
 
 	if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
 		// panacek a mince se prekryvaji
-		console.log("mince v kapse!");
 		novaMince();
 		prehrajSebraniMince();
 		prictiBod();
@@ -183,12 +151,19 @@ function prehrajSebraniMince(){
 let aktualniSkore = 0;
 let zobrazeniSkore = document.querySelector('#score');
 
-// Musím měnit text v HTML
-console.log("aktualni skore " + aktualniSkore);
-
 function prictiBod(){
 	aktualniSkore=aktualniSkore+1;
-	console.log("aktualni skore " + aktualniSkore);
 	zobrazeniSkore.innerHTML=aktualniSkore;
+	zkontrolujVyhru();
+}
+
+//Kontrola výhry (5 bodů)
+let prehraniFanfary = document.querySelector('#zvukfanfara');
+
+function zkontrolujVyhru(){
+	if (aktualniSkore === 5){
+		prehraniFanfary.play();
+		alert("GRATULUJI, PRÁVĚ JSI VYHRÁL! Klidně ale můžeš pokračovat ve hře jen tak.")
+	}
 }
 
